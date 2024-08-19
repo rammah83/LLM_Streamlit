bf33432785526cca187b8c2d537bea268c71faaa
rammah83 <rahimi.m.hicham@gmail.com>
2024-08-19 01:46:42 +0100

    fix: reorganize layout

--- app_pages/text.py
+++ app_pages/text.py
@@ -1,7 +1,9 @@
+from pyexpat import model
 from huggingface_hub import HfApi
 from pprint import pprint
 import streamlit as st
 
+# Create the instance of the API
 api = HfApi()
 tasks = [
     "sentiment-analysis",
@@ -11,14 +13,6 @@ tasks = [
     "translation_en_to_fr",
     "text-generation",
 ]
-
-with st.sidebar:
-    task = st.selectbox("Choose Tasks", tasks)
-    key_sort = st.radio("Sortby", ["downloads", "likes"])
-    limit = st.slider("limit", min_value=5, max_value=100, value=5, step=1)
-
-# Create the instance of the API
-
 
 # Return the filtered list from the Hub
 @st.cache_data
@@ -41,10 +35,25 @@ def get_models(tasks="text-classification", sort_by="likes") -> list[dict]:
     ]
 
 
-models_list = get_models(task, sort_by=key_sort)[:limit]
-models_list = sorted(models_list, key=lambda x: x["likes" if key_sort=="downloads" else key_sort], reverse=True)
 
 
-with st.expander("view models info", icon="ℹ️"):
-    st.dataframe(models_list, use_container_width=True)
-st.selectbox("choose Model", [model["id"] for model in models_list])
+model_container = st.container()
+col1, col2 = model_container.columns([1,3])
+with st.sidebar:
+    task = st.selectbox("Choose Tasks", tasks)
+    if st.checkbox("Explorer Models"):
+        key_sort = col1.radio("Sortby", ["downloads", "likes"])
+        limit = col1.slider("limit", min_value=5, max_value=20, value=5, step=1)
+        # model_container.expander("view models info", icon="ℹ️")
+        models_list = get_models(task, sort_by=key_sort)[:limit]
+        col2.dataframe(models_list, use_container_width=True)
+    else:
+        key_sort='downloads'
+        limit = 5
+
+    
+    models_list = sorted(models_list, key=lambda x: x["likes" if key_sort=="downloads" else key_sort], reverse=True)
+
+
+with st.sidebar:
+    st.selectbox("choose Model", [model["id"] for model in models_list])
