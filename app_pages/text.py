92f61e347eae65661060334722cadbed7ce30338
rammah83 <rahimi.m.hicham@gmail.com>
2024-08-20 14:02:48 +0100

    fix: extract the of creation of models and sort by last_modification in text.py

--- app_pages/text.py
+++ app_pages/text.py
@@ -1,4 +1,5 @@
 from huggingface_hub import HfApi
+from datetime import datetime
 import streamlit as st
 
 # Create the instance of the API
@@ -27,12 +28,10 @@ def get_models(tasks="text-classification", sort_by="likes") -> list[dict]:
             "id": model.modelId,
             "downloads": model.downloads,
             "likes": model.likes,
-            "created_at": model.created_at,
+            "created_at": model.created_at.date(),
         }
         for model in models
     ]
-
-
 
 # models_list = get_models(tasks="text-classification", sort_by="downloads")
 
@@ -42,10 +41,15 @@ with st.sidebar:
     task = st.selectbox("Choose Tasks", tasks)
     container_model_selector = st.container()
     with st.popover("Explorer Models"):
-        key_sort = st.radio("Sortby", ["downloads", "likes", "created_at"])
+        key_sort = st.radio("Sortby", ["downloads", "likes", "last_modified"])
         limit = st.slider("Limit to", min_value=5, max_value=20, value=5, step=5)
         # model_container.expander("view models info", icon="ℹ️")
         models_list = get_models(task, sort_by=key_sort)[:limit]
 
     container_model_selector.selectbox("choose Model", [model["id"] for model in models_list])
-st.dataframe(models_list, use_container_width=True)
+with st.expander("Show models tables", expanded=True):
+    st.dataframe(models_list, use_container_width=True)
+
+st.chat_input("you say:")
+
+
