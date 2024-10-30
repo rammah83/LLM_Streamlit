39b8e9b56674748cc5a192273e18c0ad834a267e
rammah83 <rahimi.m.hicham@gmail.com>
2024-10-30 21:18:57 +0100

    feat: fix resolution by sympy on propositional statements

--- app_pages/logic_solver.py
+++ app_pages/logic_solver.py
@@ -2,7 +2,7 @@ from operator import index
 from unittest import result
 from regex import P
 import sympy as sp
-from sympy.logic.boolalg import simplify_logic
+from sympy.logic.boolalg import simplify_logic, to_cnf
 from sympy.logic.inference import satisfiable, valid, PropKB
 
 # from string import ascii_lowercase
@@ -92,24 +92,33 @@ if len(st.session_state.selectboxes) >= 1:
     )
     st.write("---")
     if st.button("SOLVE"):
-        l:PropKB = PropKB()
-        col_sentence, col_result, col_inference = st.columns(
-            [2, 1, 4], gap="small", vertical_alignment="top"
+        kb:PropKB = PropKB()
+        col_sentence, col_result, col_validity = st.columns(
+            [2, 1, 1], gap="small", vertical_alignment="top"
         )
+        col_sentence.write("_Statement:_")
+        col_result.write("_Statement Evaluation:_")
+        col_validity.write("_Resolution:_")
         for statement in logical_statement.split("\n"):
-            # st.latex(sp.sympify(statement).atoms())
+            # display and List KB
             symp_statement = sp.sympify(statement)
+            col_sentence.latex(sp.latex(symp_statement))
             symp_result = symp_statement.subs(
                 {f"{k}": validites[f"{k}"] for k in symp_statement.atoms()}
             )
-            col_sentence.latex(sp.latex(symp_statement))
             col_result.latex(symp_result)
-            
-            simple_statement = simplify_logic(symp_statement, force=True , deep=True)
-            satisfiable_result = satisfiable(symp_statement)
-            col_inference.latex(satisfiable_result)
-            l.tell(simple_statement)
+            if valid(symp_statement):
+                col_validity.success("Valid in all possibilities")
+            else:
+                models = satisfiable(symp_statement, all_models=True)
+                if all(models):
+                    with col_validity.popover("Valid in some possibilities", use_container_width=True):
+                        for model in satisfiable(symp_statement, all_models=True):
+                            st.latex(model if model else "Impossible")
+                else:
+                    col_validity.error("Invalid in all possibilities")
+            # l.tell(simple_statement)
 
         st.write("---")
-        st.write(l.ask(l.clauses[-1]))
+        # st.write(l.ask(l.clauses[-1]))
             
